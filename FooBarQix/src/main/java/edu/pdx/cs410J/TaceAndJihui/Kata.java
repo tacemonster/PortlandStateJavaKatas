c6b30e5ebfa49aef4e542114c452e5f9f7d8f434
Tacy Bechtel <tacy.bechtel@gmail.com>
2022-07-06 20:28:40 -0700

    Finished code implementation and unit tests for FooBarQix

--- FooBarQix/src/main/java/edu/pdx/cs410J/TaceAndJihui/Kata.java
+++ FooBarQix/src/main/java/edu/pdx/cs410J/TaceAndJihui/Kata.java
@@ -9,10 +9,41 @@ package edu.pdx.cs410J.TaceAndJihui;
 public class Kata {
 
   public static void main(String[] args) {
-    System.err.println("Missing command line arguments");
+    if (args.length == 0) {
+      System.err.println("Missing command line arguments");
+      return;
+    }
+    String result = compute(args[0]);
+    System.out.println(result);
   }
 
-  public static String compute(String value) {
-    
+  public static String compute(String s) {
+    int value = Integer.parseInt(s);
+    String returnStr = "";
+    if (value % 3 == 0) {
+      returnStr += "Foo";
+    }
+    if (value % 5 == 0) {
+      returnStr += "Bar";
+    }
+    if (value % 7 == 0) {
+      returnStr += "Qix";
+    }
+    for (int i = 0; i < s.length(); i++) {
+      if (s.charAt(i) == '3') {
+        returnStr += "Foo";
+      } else if (s.charAt(i) == '5') {
+        returnStr += "Bar";
+      } else if (s.charAt(i) == '7') {
+        returnStr += "Qix";
+      } else if (s.charAt(i) == '0') {
+        returnStr += "*";
+      }
+    }
+    if (returnStr.equals("") || returnStr.matches("[\\*]+")) {
+      returnStr = s;
+      returnStr = returnStr.replace('0', '*');
+    }
+    return returnStr;
   }
 }
--- FooBarQix/src/test/java/edu/pdx/cs410J/TaceAndJihui/KataTest.java
+++ FooBarQix/src/test/java/edu/pdx/cs410J/TaceAndJihui/KataTest.java
@@ -2,6 +2,10 @@ package edu.pdx.cs410J.TaceAndJihui;
 
 import org.junit.jupiter.api.Test;
 
+import static edu.pdx.cs410J.TaceAndJihui.Kata.compute;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
 public class KataTest
 {
 
@@ -11,8 +15,51 @@ public class KataTest
   }
 
   @Test
+  void oneReturnsOne() {
+    assertThat(compute("1"), equalTo("1"));
+  }
+  @Test
   void divisibleByThreeReturnsFoo() {
-    
+    assertThat(compute("6"), equalTo("Foo"));
   }
 
+  @Test
+  void divisibleByFiveReturnsFoo() {
+    assertThat(compute("10"), equalTo("Bar"));
+  }
+
+  @Test
+  void divisibleBySevenReturnsFoo() {
+    assertThat(compute("14"), equalTo("Qix"));
+  }
+
+  @Test
+  void threeReturnsFooFoo() {
+    assertThat(compute("3"), equalTo("FooFoo"));
+  }
+
+  @Test
+  void fiveReturnsBarBar() {
+    assertThat(compute("5"), equalTo("BarBar"));
+  }
+
+  @Test
+  void sevenReturnsQixQix() {
+    assertThat(compute("7"), equalTo("QixQix"));
+  }
+
+  @Test
+  void testComplexResults() {
+    assertThat(compute("15"), equalTo("FooBarBar"));
+    assertThat(compute("21"), equalTo("FooQix"));
+    assertThat(compute("33"), equalTo("FooFooFoo"));
+  }
+
+  @Test
+  void okayNowWeAddedTheZeroThing() {
+    assertThat(compute("101"), equalTo("1*1"));
+    assertThat(compute("303"), equalTo("FooFoo*Foo"));
+    assertThat(compute("105"), equalTo("FooBarQix*Bar"));
+    assertThat(compute("10101"), equalTo("FooQix**"));
+  }
 }
